f6b4ba1967876fe0563aaf68ee4721727cbf9adc
Benjamin Seamons <Masterofart605@gmail.com>
2022-09-15 16:56:30 -0600

    Finished song(at 1/8th speed)

---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿int A = 440;
+int B = 490;
+int C = 262;
+int D = 294;
+int E = 330;
+int F = 349;
+int G = 392;
+
+int A2 = 880;
+int B2 = 987;
+int C2 = 523;
+int D2 = 587;
+int E2 = 659;
+int F2 = 698;
+int G2 = 783;
+
+int wholeNote = 1000;
+int eightNote = wholeNote;
+
+
+
+Console.Beep(G,eightNote);
+Console.Beep(C2,eightNote);
+Console.Beep(D2,eightNote);
+Console.Beep(E2,eightNote);
+Console.Beep(D2,eightNote);
+Console.Beep(C2,eightNote);
+Console.Beep(D2,eightNote);
+Console.Beep(E2,eightNote);
+Console.Beep(C2,eightNote);
+
+Console.Beep(C2,eightNote);
+Console.Beep(C2,eightNote);
+Console.Beep(C2,eightNote);
+Console.Beep(C2,eightNote);
+
+Console.Beep(G,eightNote);
+Console.Beep(C2,eightNote);
+Console.Beep(D2,eightNote);
+Console.Beep(E2,eightNote);
+Console.Beep(D2,eightNote);
+Console.Beep(C2,eightNote);
+Console.Beep(D2,eightNote);
+Console.Beep(E2,eightNote);
+Console.Beep(C2,eightNote);
+
+Console.Beep(C2,eightNote);
+Console.Beep(C2,eightNote);
+Console.Beep(C2,eightNote);
+Console.Beep(C2,eightNote);
+
+Console.Beep(A2,eightNote);
+Console.Beep(D2,eightNote);
+Console.Beep(E2,eightNote);
+Console.Beep(D2,eightNote);
+Console.Beep(A2,eightNote);
+Console.Beep(G2,eightNote);
+Console.Beep(A2,eightNote);
+Console.Beep(D2,eightNote);
+Console.Beep(E2,eightNote);
+Console.Beep(D2,eightNote);
+Console.Beep(E2,eightNote);
+Console.Beep(D2,eightNote);
+Console.Beep(B2,eightNote);
+
+Console.Beep(D2,eightNote);
+Console.Beep(D2,eightNote);
+Console.Beep(D2,eightNote);
+Console.Beep(D2,eightNote);
+
+Console.Beep(G,eightNote);
+Console.Beep(C2,eightNote);
+Console.Beep(D2,eightNote);
+Console.Beep(C2,eightNote);
+Console.Beep(G,eightNote);
+Console.Beep(E,eightNote);
+Console.Beep(G,eightNote);
+Console.Beep(C2,eightNote);
+Console.Beep(D2,eightNote);
+Console.Beep(C2,eightNote);
+Console.Beep(G,eightNote);
+
+Console.Beep(C2,eightNote);
+Console.Beep(C2,eightNote);
+Console.Beep(C2,eightNote);
+Console.Beep(C2,eightNote);
+
+Console.Beep(G,eightNote);
+Console.Beep(C2,eightNote);
+Console.Beep(D2,eightNote);
+Console.Beep(E2,eightNote);
+Console.Beep(D2,eightNote);
+Console.Beep(C2,eightNote);
+Console.Beep(D2,eightNote);
+Console.Beep(E2,eightNote);
+Console.Beep(C2,eightNote);
+
+Console.Beep(C2,eightNote);
+Console.Beep(C2,eightNote);
+Console.Beep(C2,eightNote);
+Console.Beep(C2,eightNote);
